d82f0e0aad16c49f6715ace0f39b7ac107308082
River Robinson <riverrob1999@gmail.com>
2020-11-29 18:42:27 -0600

    Initial commit with basic classes.

---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+public class Menu {
+
+    public static Double getDouble(String prompt){
+        //TODO Write code to get a valid double.
+        return 0.0;
+    }
+
+    public static String getString(String prompt){
+        //TODO Write code to get a valid string.
+        return "";
+    }
+}
